cdc8d3c23b2564c36569be417bb9683c6cb8f2cd
Johnson Chen <johnsonchen12345@gmail.com>
2019-01-26 00:41:07 +0000

    Home

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden"; // class "loader hidden"
-});
+    window.addEventListener("load", function() {
+        const loader = document.querySelector(".loader");
+        loader.className += " hidden"; // class "loader hidden"
+    });
 });
